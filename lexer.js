abbcb236b01260e9c0823cc2efdf98a4d46448f6
rabby-shek <rabbyshek1998@gmail.com>
2025-03-22 20:12:54 +0600

    working on lexer file functionalities

--- lexer.js
+++ lexer.js
@@ -9,10 +9,28 @@ var TokenType;
     TokenType[TokenType["Punctuation"] = 6] = "Punctuation";
     TokenType[TokenType["Whitespace"] = 7] = "Whitespace";
     TokenType[TokenType["Comment"] = 8] = "Comment";
+    TokenType[TokenType["Let"] = 9] = "Let";
 })(TokenType || (TokenType = {}));
+var KEYWORDS = {
+    let: TokenType.Let,
+};
+//  create a token
 function token(value, type) {
     if (value === void 0) { value = ""; }
     return { value: value, type: type };
+}
+// check if a character is a letter
+function isAlpha(src) {
+    return src.toLowerCase() !== src.toUpperCase();
+}
+// check if a character is a digit
+function isInt(src) {
+    var c = src.charCodeAt(0);
+    var bounds = ["0".charCodeAt(0), "9".charCodeAt(0)];
+    return c >= bounds[0] && c <= bounds[1];
+}
+function isSkipAble(src) {
+    return src === " " || src === "\n" || src === "\t";
 }
 function tokenize(sourceCode) {
     var tokens = [];
@@ -30,19 +48,40 @@ function tokenize(sourceCode) {
         else if (src[0] === "=") {
             tokens.push(token(src.shift(), TokenType.AssignmentOperator));
         }
+        else {
+            // handle multi character tokens
+            // build number token
+            if (isInt(src[0])) {
+                var num = "";
+                while (src.length > 0 && isInt(src[0])) {
+                    num += src.shift();
+                }
+                tokens.push(token(num, TokenType.Number));
+            }
+            else if (isAlpha(src[0])) {
+                var Identifier = "";
+                while (src.length > 0 && isAlpha(src[0])) {
+                    Identifier += src.shift();
+                }
+                // check if the identifier is a keyword
+                if (KEYWORDS[Identifier] === undefined) {
+                    tokens.push(token(Identifier, TokenType.Identifier));
+                }
+                else {
+                    tokens.push(token(Identifier, KEYWORDS[Identifier]));
+                }
+            }
+            else if (isSkipAble(src[0])) {
+                src.shift();
+            }
+            else {
+                console.log("Unknown token", src[0]);
+                process.exit(1);
+            }
+        }
     }
     return tokens;
 }
-var code = "++=";
+var code = "let x = 55 + 45";
 var tokens = tokenize(code);
 console.log(tokens);
-// document.getElementById("run")?.addEventListener("click", () => {
-//   const code = (document.getElementById("codeEditor") as HTMLTextAreaElement)
-//     .value;
-//   const tokens = tokenize(code);
-//   document.getElementById("output")!.innerText = JSON.stringify(
-//     tokens,
-//     null,
-//     2
-//   );
-// });
--- lexer.ts
+++ lexer.ts
@@ -8,18 +8,35 @@ enum TokenType {
   Punctuation,
   Whitespace,
   Comment,
+  Let,
 }
+const KEYWORDS: Record<string, TokenType> = {
+  let: TokenType.Let,
+};
 
 interface Token {
   type: TokenType;
   value: string;
 }
+//  create a token
 function token(value = "", type: TokenType): Token {
   return { value, type };
 }
+// check if a character is a letter
 function isAlpha(src: string): boolean {
   return src.toLowerCase() !== src.toUpperCase();
 }
+// check if a character is a digit
+function isInt(src: string): boolean {
+  const c = src.charCodeAt(0);
+  const bounds = ["0".charCodeAt(0), "9".charCodeAt(0)];
+  return c >= bounds[0] && c <= bounds[1];
+}
+
+function isSkipAble(src: string): boolean {
+  return src === " " || src === "\n" || src === "\t";
+}
+
 function tokenize(sourceCode: string): Token[] {
   const tokens: Token[] = [];
   const src = sourceCode.split("");
@@ -36,22 +53,38 @@ function tokenize(sourceCode: string): Token[] {
       tokens.push(token(src.shift(), TokenType.ArithmeticOperator));
     } else if (src[0] === "=") {
       tokens.push(token(src.shift(), TokenType.AssignmentOperator));
+    } else {
+      // handle multi character tokens
+
+      // build number token
+      if (isInt(src[0])) {
+        let num = "";
+        while (src.length > 0 && isInt(src[0])) {
+          num += src.shift();
+        }
+        tokens.push(token(num, TokenType.Number));
+      } else if (isAlpha(src[0])) {
+        let Identifier = "";
+        while (src.length > 0 && isAlpha(src[0])) {
+          Identifier += src.shift();
+        }
+        // check if the identifier is a keyword
+        if (KEYWORDS[Identifier] === undefined) {
+          tokens.push(token(Identifier, TokenType.Identifier));
+        } else {
+          tokens.push(token(Identifier, KEYWORDS[Identifier]));
+        }
+      } else if (isSkipAble(src[0])) {
+        src.shift();
+      } else {
+        console.log("Unknown token", src[0]);
+        process.exit(1);
+      }
     }
   }
   return tokens;
 }
 
-const code = "++=";
+const code = "let x = 55 + 45";
 const tokens = tokenize(code);
 console.log(tokens);
-
-// document.getElementById("run")?.addEventListener("click", () => {
-//   const code = (document.getElementById("codeEditor") as HTMLTextAreaElement)
-//     .value;
-//   const tokens = tokenize(code);
-//   document.getElementById("output")!.innerText = JSON.stringify(
-//     tokens,
-//     null,
-//     2
-//   );
-// });
