ad3bc79d3517d699281718f2a773668bf8a2f505
rabby-shek <rabbyshek1998@gmail.com>
2025-08-04 18:49:45 +0600

    restarting the project

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as fs from 'fs';
-import { tokenize } from "./lexer/tokenizer";
-import path from "path";
-const filePath = path.resolve(__dirname, "index.nova");
-const code = fs.readFileSync(filePath, "utf-8");
-const tokens = tokenize(code);
-console.log(tokens);
